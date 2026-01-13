66891df63b743d0d9e81538e679a2b756866a851
bobothefool <mehreensajidRS@gmail.com>
2026-01-13 10:10:08 -0500

    interating through fruits using list comprehension

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # save using Cmd + S
 print("fruit")
 fruits = ["apple", "banana", "cantaloupe", "dragonfruit"]
-for fruit in fruits: 
-    print(fruit)
+output = [fruit for fruit in fruits]
+print(output)
