b6ccbebc45cb0bb03ce33b66c139b31caf32b1aa
bobothefool <mehreensajidRS@gmail.com>
2026-01-13 10:42:02 -0500

    prompts user to enter a fruit, checks if it's a valid fruit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 # save using Cmd + S
-print("fruit")
 fruits = ["apple", "banana", "cantaloupe", "dragonfruit"]
 output = [fruit for fruit in fruits]
 print(output)
+
+user_fruit = input("what is your favorite fruit?: ")
+if user_fruit in fruits: 
+    print("that is a fruit!")
+else: 
+    print("that's not a fruit. the valid fruits are " + ", ".join(fruits))
